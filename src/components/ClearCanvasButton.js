c972b1969aac31ab25550d35b7a6dba038a99ae7
00kang <kdb070970@gmail.com>
2022-11-05 16:46:33 +0900

    [#4] canvas 위치 조정, 리셋  버튼 완료

--- src/components/ClearCanvasButton.js
+++ src/components/ClearCanvasButton.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { useCanvas } from "./CanvasContext";
+import styles from "../styles/Draw.module.css";
 
 export const ClearCanvasButton = () => {
   const { clearCanvas } = useCanvas();
 
-  return <button onClick={clearCanvas}>지우기</button>;
+  return (
+    <button onClick={clearCanvas} className={styles.clearDrawing}>
+      리셋
+    </button>
+  );
 };
