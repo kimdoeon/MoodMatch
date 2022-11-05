4a81457bb725602850dc9471b07d32e6496a9614
00kang <kdb070970@gmail.com>
2022-11-05 15:34:48 +0900

    [#4] canvas 추가 (그림, 지우기 보완 필요)

--- src/components/Draw.js
+++ src/components/Draw.js
@@ -1,41 +1,45 @@
-import React, { useState } from 'react';
-import {Link} from "react-router-dom";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "../styles/Draw.module.css";
-// import {useState,useEffect} from "react";
+// import Drawing from "./Drawing";
+import { Canvas } from "./Canvas";
+import { ClearCanvasButton } from "./ClearCanvasButton";
+import { CanvasProvider } from "./CanvasContext";
+
 function Draw() {
   return (
-   <div>
-    <header className={styles.draw_Header}>
-              <Link to="/">
-                <img src ="./img/logo_kids.png" alt="logo"/>
-              </Link>
-            </header>
-     <section className={styles.main}>
+    <div>
+      <header className={styles.draw_Header}>
+        <Link to="/">
+          <img src="./img/logo_kids.png" alt="logo" />
+        </Link>
+      </header>
+      <section className={styles.main}>
         <div className={styles.container}>
-            <div className={styles.title}>
-                내 그림과 어울리는 음악 추천 받기
-            </div>
-            
-            <div className={styles.btn_drawing}>
-                <i className="fa-solid fa-pencil"></i>
-                <i className="fa-solid fa-eraser"></i>
-            </div>
-            <div className={styles.upload_drawing}>
-                마음껏 그림을 그려보세요
-            </div>
+          <div className={styles.title}>내 그림과 어울리는 음악 추천 받기</div>
 
-   
+          <div className={styles.btn_drawing}>
+            <i className="fa-solid fa-pencil"></i>
+            <i className="fa-solid fa-eraser"></i>
+          </div>
 
-            <Link to="/recommend">
-                <button>완성</button>
-            </Link>
+          <div className={styles.upload_drawing}>
+            <CanvasProvider>
+              <Canvas />
+              <ClearCanvasButton />
+            </CanvasProvider>
+          </div>
 
-            <div className={styles.tip}>
-                TIP! 내가 표현하고 싶은 것을 단어와 함께 표현해보세요~
-            </div> 
+          <Link to="/recommend">
+            <button>완성</button>
+          </Link>
+
+          <div className={styles.tip}>
+            TIP! 내가 표현하고 싶은 것을 단어와 함께 표현해보세요~
+          </div>
         </div>
-    </section>
-   </div>
+      </section>
+    </div>
   );
 }
 
